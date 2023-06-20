647350a571b200b64b287c2692f70c14c5727697
Owen Arts <owen.arst@hotmail.com>
2023-06-20 11:32:00 +0200

    scaled the hitboxes down to the right size

--- GameManager.cpp
+++ GameManager.cpp
@@ -47,7 +47,7 @@ void GameManager::spawnEnemy()
 	enemy->rotation.y = -camera->rotation.y;
 	enemy->addComponent(std::make_shared<ModelComponent>(models[getRandomEnemy()]));
 	enemy->addComponent(std::make_shared<MoveEnemyComponent>(camera));
-	enemy->addComponent(std::make_shared<BoundingBoxComponent>(glm::vec3(-6, 0, -6), glm::vec3(6, 10, 6)));
+	enemy->addComponent(std::make_shared<BoundingBoxComponent>(glm::vec3(-1, 2, -1), glm::vec3(1, 4.5, 1)));
 	objects.push_back(enemy);
 	aliveEnemies++;
 }
